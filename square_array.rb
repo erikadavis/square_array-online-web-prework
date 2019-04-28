7febb430a2f65c7258c589327c99e085ffc88561
Erika Davis <davis2erika@gmail.com>
2019-04-28 01:37:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,5 @@ def square_array(array)
   squared_elements = Array.new
   array.each do |number|
     puts number ** 2
-  end
-    return squared_elements << number ** 2
+      return squared_elements << number ** 2
   end
